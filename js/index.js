0a322ba7f4681f85e8b67940adb42cf541287bb6
Kevin <kelvinngechu@gmail.com>
2022-12-20 14:25:14 +0300

    fetch user repo

--- js/index.js
+++ js/index.js
@@ -20,4 +20,28 @@ function handleSearch() {
     .then(data => {console.log(data)
         document.querySelector('#user-list').innerHTML = ''
         document.querySelector('#repos-list').innerHTML =''
+        data.items.forEach(user => {
+            console.log(user)
+            let userCard = document.createElement('li')
+            userCard.className = 'all-users'
+            userCard.innerHTML = `
+                <div class='content'>
+                    <h3> User: ${user.login}</h3>
+                    <p> URL: ${user.html_url}</p>
+                    <div class ='repos'>
+                    <button class='repo-button' style='margin-bottom: 25px'>
+                    Repositories
+                    </button>
+                    </div>
+                    <img src=${user.avatar_url} />
+                </div>`
+            
+           document.querySelector('#user-list').appendChild(userCard)   
+           
+           const repoButton = document.querySelector('.repo-button')
+           console.log(repoButton)
+           repoButton.addEventListener('click', () => {
+               fetch(user.repos_url, {
+               method: 'GET',
+               header:{
   
